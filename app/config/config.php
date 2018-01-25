2b336cc57dbb7808feb965f52b7ac45834dc9d79
jibanez74 <jibanez.romany74@gmail.com>
2018-01-25 16:53:14 -0400

    bootstrap.php noew auto-load all libs

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+//define the root of app
+define(
+  "APPROOT",
+  dirname(dirname("__FILE__"))
+);
+//set url
+define(
+  "URLROOT",
+  "http://localhost/PHP_MVC"
+);
+//define the site name
+define(
+  "SITENAME",
+  "PHP_MVC"
+)
